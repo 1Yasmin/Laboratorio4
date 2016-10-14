199701a15cbf1f0774d09b347f5cfb87712277d0
1Yasmin <ychavez2014ig@gmail.com>
2016-10-14 09:07:38 -0600

    Resolucion Ejercicio 2

---
+++ src/MainEjercicio2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author chavez
+ *
+ */
+public class MainEjercicio2 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Alarma2 miReloj = new Alarma2();
+		System.out.println(miReloj);
+		miReloj.m1();
+		miReloj.m2();
+
+	}
+
+}
