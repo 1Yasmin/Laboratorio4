960267e97437b9ed6f53f6a44f428b35317eea2b
1Yasmin <ychavez2014ig@gmail.com>
2016-10-14 08:26:25 -0600

    timbre

--- src/Timbre.java
+++ src/Timbre.java
@@ -1,18 +1,20 @@
 /**
  * 
- */
-
-/**
  * @author chavez
  *
  */
-public class Timbre {
 
-	/**
-	 * 
-	 */
-	public Timbre() {
-		// TODO Auto-generated constructor stub
+
+public class Timbre {
+	public void m1() {
+		System.out.println("timbre 1");
 	}
 
+	public void m2() {
+		System.out.println("timbre 2");
+	}
+
+	public String toString() {
+		return "Ring…Ring";
+	}
 }
