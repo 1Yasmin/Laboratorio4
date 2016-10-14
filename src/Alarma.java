a57fc219113ffb10bc8e2f3eab85033f6cd35d0c
1Yasmin <ychavez2014ig@gmail.com>
2016-10-14 08:18:25 -0600

    iniciando

---
+++ src/Alarma.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+/**
+ * @author chavez
+ *
+ */
+
+public class Alarma extends Timbre {
+	public void m1() {
+		System.out.println("alarma 1");
+	}
+	 
+}
---
+++ src/Timbre.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author chavez
+ *
+ */
+public class Timbre {
+
+	/**
+	 * 
+	 */
+	public Timbre() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
